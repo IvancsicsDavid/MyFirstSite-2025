ba4235745c2702308a16a06a0d44a93e95b5c24f
IvancsicsDavid <ivancsicsdavid05@gmail.com>
2025-09-15 13:21:18 +0200

    valami

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return "hello world!";
+});
+
+Route::get('/valami', function () {
+    return view('valami');
+});
